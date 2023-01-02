f2e9c1cfc376c0b24041721e44f1bb1e4537631d
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2023-01-02 12:52:39 +0700

    login

---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Login Ges</title>
+    </head>
+    <body>
+        <h1>Login Aplikasi</h1>
+        <form action="proses.php" method="post">
+            <table>
+                <tr>
+                    <td>Username</td>
+                    <td>:</td>
+                    <td><input type="text" name="user" id="user"></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td>:</td>
+                    <td><input type="password" name="pass" id="pass"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td><input type="submit" value="login"></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
---
+++ proses.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+// Menangkap data yang dikirim dari login
+$username = $_POST['user'];
+$password = $_POST['pass'];
+
+// Menyeleksi data dan dicocokan pada table admin xampp
+$data = mysqli_query($koneksi,"select * from user where username='$username' and password='$password'");
+
+// Menghitung jumlah data yang ditemukan
+$cek_data = mysqli_num_rows($data);
+
+if($cek_data > 0){
+    $_SESSION['user']=$username;
+    $_SESSION['status']='login';
+    header("location:admin/index.php");
+}else{
+    header("location:login.php?pesan=gagal");
+}
