6aab3ad4d201c6855f263f65fd85d4066985c837
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2023-01-02 13:22:56 +0700

    2

--- user/index.php
+++ user/index.php
@@ -6,6 +6,16 @@
         <h1>Selamat Datang Di Index</h1>
         <h3>Halaman User</h3>
 
+        <!-- Cek halaman apakah sudah login atau belum -->
+        <?php
+        session_start();
+        if($_SESSION['status']!='login'){
+            header('location:../login.php?pesan=belum_login');
+        }
+        ?>
+        <!-- end -->
+        
+        <h4>Welcome <?php echo $_SESSION['user']?> anda telah login</h4>
         <a href="../logout.php">LOGOUT</a>
     </body>
 </html>
